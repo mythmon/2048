1f1cef9a357a81beb089706ede32bb1af1d068eb
Mike Cooper <mythmon@gmail.com>
2014-04-07 18:07:09 -0700

    Shitty AI in a worker thread.

--- js/application.js
+++ js/application.js
@@ -1,4 +1,6 @@
+var manager;
+
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+  manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
 });
